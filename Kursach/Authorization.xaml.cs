dba8a88dfb05bc76d3653f0c6fd5ff4d84f2ea92
grapes <kamalov_bulat2002@mail.ru>
2021-05-24 17:17:59 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Authorization.xaml.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        u1255664_testdriveEntities db = new u1255664_testdriveEntities();
+
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterToAccount(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLoginAuth.Text.Trim();
+            string pass = textBoxPassAuth.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLoginAuth.ToolTip = "Это поле введено не корректно!";
+                textBoxLoginAuth.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                textBoxPassAuth.ToolTip = "Это поле введено не корректно!";
+                textBoxPassAuth.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLoginAuth.ToolTip = "";
+                textBoxLoginAuth.Background = Brushes.Transparent;
+                textBoxPassAuth.ToolTip = "";
+                textBoxPassAuth.Background = Brushes.Transparent;
+
+                Account account12 = null;
+                using(u1255664_testdriveEntities db = new u1255664_testdriveEntities())
+                {
+                    account12 = db.Accounts1.Where(b => b.Login == login && b.Password == pass).FirstOrDefault();
+                }
+                MessageBox.Show("Вход выполнен!");
+            }
+        }
+    }
+}
---
+++ Kursach/Registration.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Linq;
+using System.Windows.Navigation;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        u1255664_testdriveEntities db = new u1255664_testdriveEntities();
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Text.Trim();
+            string passAgain = passBoxAgain.Text.Trim();
+
+            //Account account = new Account()
+            //{
+            //    Login = textBoxLogin.Text,
+            //    Password = passBox.Text
+            //};
+
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != passAgain)
+            {
+                passBoxAgain.ToolTip = "Данные не совпадают!";
+                passBoxAgain.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBoxAgain.ToolTip = "";
+                passBoxAgain.Background = Brushes.Transparent;
+
+
+                MessageBox.Show("Все отлично!");
+
+                Account account1 = new Account(login, pass);
+                
+                db.Accounts1.Add(account1);
+                db.SaveChanges();
+                //NavigationService.Navigate(new Authorization());
+
+
+                #region actual
+                //var countLogin = from Account in db.Accounts1
+                //                 where Account.Login == login
+                //                 select Account;
+
+                //if (countLogin.Count() > 0)
+                //{
+                //    MessageBox.Show("Данный пользователь с таким логином уже существует. Попробуйте другой!");
+                //}
+                //else
+                //{
+                //    db.Accounts1.Add(account);
+                //    db.SaveChanges();
+                //    MessageBox.Show("Регистрация прошла успешно!");
+                //    NavigationService.GetNavigationService(new Authorization());
+                //}
+                #endregion
+
+                #region noActual
+                //Account account = new Account(login, pass);
+
+                //using (test_driveEntities1 db = new test_driveEntities1())
+                //{
+                //db.Accounts1.Add(new Account(login, pass));
+                //db.SaveChanges();
+                //}
+
+                //test_driveEntities1 db = new test_driveEntities1();
+
+                //db.Accounts1.Add(new Account());
+                //db.SaveChanges();
+
+                //MessageBox.Show("Регистрация прошла успешно!");
+                #endregion
+            }
+        }
+    }
+}
